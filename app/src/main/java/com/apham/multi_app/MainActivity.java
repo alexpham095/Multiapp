167ecebb2b26721b2fc004b0b84936a76b04c895
Alex Pham <alexpham095@gmail.com>
2018-10-22 15:14:01 -0700

    Test Commit 1

---
+++ app/src/main/java/com/apham/multi_app/MainActivity.java
@@ -0,0 +1,73 @@
+package com.apham.multi_app;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    int currentItem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer happySound = MediaPlayer.create(this, R.raw.cheering);
+        Button playHappySound = (Button) this.findViewById(R.id.play_happy);
+        playHappySound.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                happySound.start();
+            }
+        });
+
+        final MediaPlayer sadSound = MediaPlayer.create(this, R.raw.crying);
+        Button playSadSound = (Button) this.findViewById(R.id.play_sad);
+        playSadSound.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                sadSound.start();
+            }
+        });
+
+
+        Spinner mySpinner = (Spinner)findViewById(R.id.spinner);
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l){
+
+                if(currentItem==position){
+                    return;
+                }
+                else if(position==1){
+                    Intent intent= new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent= new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView){
+
+            }
+        });
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                R.layout.spinner_item, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(R.layout.spinner_item);
+        mySpinner.setAdapter(myAdapter);
+
+
+    }
+}
---
+++ app/src/main/java/com/apham/multi_app/SecondActivity.java
@@ -0,0 +1,194 @@
+package com.apham.multi_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
+            buttonMul, button10, buttonC, buttonEqual;
+    EditText editText;
+
+    float a, b;
+
+    boolean addition, subt, multi, div;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button10 = (Button) findViewById(R.id.button10);
+        buttonAdd = (Button) findViewById(R.id.buttonadd);
+        buttonSub = (Button) findViewById(R.id.buttonsub);
+        buttonMul = (Button) findViewById(R.id.buttonmult);
+        buttonDivision = (Button) findViewById(R.id.buttondiv);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonEqual = (Button) findViewById(R.id.buttoneql);
+        editText = (EditText) findViewById(R.id.edt1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "3");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "4");
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "5");
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "6");
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "8");
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "8");
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "9");
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "0");
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(editText ==null){
+                    editText.setText("");
+                } else {
+                    a = Float.parseFloat(editText.getText() + "");
+                    addition = true;
+                    editText.setText(null);
+                }
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = Float.parseFloat(editText.getText() + "");
+                subt = true;
+                editText.setText(null);
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = Float.parseFloat(editText.getText() + "");
+                multi = true;
+                editText.setText(null);
+            }
+        });
+
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = Float.parseFloat(editText.getText() + "");
+                div = true;
+                editText.setText(null);
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b = Float.parseFloat(editText.getText() + "");
+                if(addition == true){
+                    editText.setText(a + b + "");
+                    addition = false;
+                }
+
+                if(subt == true){
+                    editText.setText(a - b + "");
+                    subt = false;
+                }
+
+                if(multi == true){
+                    editText.setText(a * b + "");
+                    multi = false;
+                }
+
+                if(div == true){
+                    editText.setText(a / b + "");
+                    div = false;
+                }
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                editText.setText(editText.getText() + ".");
+            }
+        });
+    }
+}
